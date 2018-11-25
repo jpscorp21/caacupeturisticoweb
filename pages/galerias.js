b31cb37b2c8f50569fb70e850ebc073706aec4c8
Julio César <jcesarsaucedo1993@gmail.com>
2018-11-25 12:13:00 -0300

    Creacion de galerias

---
+++ pages/galerias.js
@@ -0,0 +1,10 @@
+import Layout from "../components/MyLayout";
+import {Container} from "reactstrap";
+const Galerias = () =>(
+    <Layout>
+        <Container>
+            <h1>Galeria de fotos</h1>
+        </Container>
+    </Layout>
+)
+export default Galerias;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import { Button, Container } from 'reactstrap';
+import Link from "next/link";
 import Layout from '../components/MyLayout';
 
 const Index = () => (
     <Layout>
         <Container>
             <h1>Inicio</h1>
-            <Button color="danger">Acerca de</Button>
+            <Link href="/galerias">
+                <Button color="danger">Galeria</Button>
+            </Link>
         </Container>
     </Layout>
 )
