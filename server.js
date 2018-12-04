6297feff8a7ec15a78ea25cc7e08da1224a254b7
Jean Paul Saucedo <jpscorp21@gmail.com>
2018-12-04 10:41:30 -0300

    ajustes del server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const next = require('next');
 
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000; 
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -23,9 +24,9 @@ app.prepare()
         return handle(req, res);
     })
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
         if (err) throw err;
-        console.log(`> Ready on http://localhost:3000`);
+        console.log(`> Ready on http://localhost:${port}`);
     })
 })
 .catch((ex) => {
